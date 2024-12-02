3fcdca439bc54493bc2b7b00e47d0904ae1de3d5
Asker <kotsevasker0@gmail.com>
2024-12-02 19:35:48 +0300

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+
 
 	"github.com/Asker231/test-hash-generate.git/internal/user"
 	"github.com/Asker231/test-hash-generate.git/pkg/writer"
@@ -9,15 +9,9 @@ import (
 
 
 func main() {
-	person := user.NewUser("Asker","Google.com");
-	person2 := user.NewUser("Jhon","Google.com");
-	fmt.Printf("Id: %s\n Name: %s\n Url:%s\n",person.Id,person.Name,person.Url)
-	err := writer.WriteHandle(*person)
-	if err !=nil{
-		fmt.Print(err)
-	}
-	err = writer.WriteHandle(*person2)
-	if err != nil{
-		fmt.Println(err)
-	}
+	person := user.NewUser("Asker","Googlse.com");
+	person2 := user.NewUser("Jhon","Googles.com");
+
+	writer.WriteHandle(*person,*person2)
+
 }
--- pkg/writer/writehandle.go
+++ pkg/writer/writehandle.go
@@ -8,26 +8,12 @@ import (
 
 
 
-func WriteHandle[T any](data T)error{
+func WriteHandle[T any](data... T){
 	arr := []T{}
-	arr = append(arr, data)	
-	users,err:=	json.Marshal(&arr)
-	if err != nil{
-		return  err
-	}
-	file,err := os.Create("db.json")
-    if err != nil{
-		return err
-	}
-	file,err = os.OpenFile(file.Name(),os.O_APPEND|os.O_CREATE,0600)
+	arr = append(arr, data...)
+	dByte,_ := json.Marshal(arr)
+	err := os.WriteFile("db.json",dByte,0644)
 	if err != nil{
 		fmt.Println(err)
 	}
-	err = os.WriteFile(file.Name(),users,0644)
-
-	if err != nil{
-		fmt.Println(err)
-	}
-	defer file.Close()
-	return nil
 }
